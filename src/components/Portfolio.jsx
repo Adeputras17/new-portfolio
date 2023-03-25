81641626d75487e7b976d10253f235b027a37b25
Adeputras17 <kirabelkparg@gmail.com>
2023-03-25 20:08:17 +0800

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -63,23 +63,16 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
 
               />
-              <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center text-center">
                 <a
                   href={href}
                   alt=''
                   target="_blank"
                   rel="noreferrer"
                   className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
+                  Github Repository 
                 </a>
-                <a
-                  href={href}
-                  alt=''
-                  target="_blank"
-                  rel="noreferrer"
-                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 ">
-                  Code
-                </a>
+                
               </div>
             </div>
           ))}
